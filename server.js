35d7c7b3905adeae87e138f7547c35a509d90a80
Glitch (oceanic-twisty-syrup) <none>
2020-07-26 19:01:30 +0000

    🎃🌻 Checkpoint ./server.js:9146631/4 ./commands/ping.js:9146631/1827

--- server.js
+++ server.js
@@ -48,13 +48,13 @@ client.on("message", async (message) => {
 
     const now = Date.now();
     const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 1) * 1000;
+    const cooldownAmount = (command.cooldown || 1) * 3000;
 
     if (timestamps.has(message.author.id)) {
       const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
       if (now < expirationTime) {
-        const timeLeft = (expirationTime - now) / 1000;
+        const timeLeft = (expirationTime - now) / 3000;
         return message.reply(
           `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`
         );
